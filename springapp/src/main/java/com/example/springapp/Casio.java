f83c5f96463a08670881d509114bdb5584660e17
neojarvis <techadmin@iamneo.ai>
2023-12-20 06:00:52 +0000

    Manual-save-commit-12/20/2023--11:30:45-AM

--- springapp/src/main/java/com/example/springapp/Casio.java
+++ springapp/src/main/java/com/example/springapp/Casio.java
@@ -22,4 +22,48 @@ public class Casio implements Calculator{
         // System.out.println(num2-num1);      
           return diff;
     }
+
+     int num1,num2;
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+    public int add() {
+        int sum;
+       sum=num1+num2;
+       //System.out.println(num1+num2);      
+        return sum;
+        // TODO Auto-generated method stub    
+    }
+    public int sub() {
+        // TODO Auto-gene      
+         int diff;
+        diff=num2-num1;
+        // System.out.println(num2-num1);      
+          return diff;
+    }
+
+     int num1,num2;
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+    public int add() {
+        int sum;
+       sum=num1+num2;
+       //System.out.println(num1+num2);      
+        return sum;
+        // TODO Auto-generated method stub    
+    }
+    public int sub() {
+        // TODO Auto-gene      
+         int diff;
+        diff=num2-num1;
+        // System.out.println(num2-num1);      
+          return diff;
+    }
 }
